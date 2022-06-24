d76b758861bbac01e41d85823e73415294e880a0
yoshiharutakemoto <85409237+yoshiharutakemoto@users.noreply.github.com>
2022-06-24 20:22:43 +0900

    Delete payroll directory

--- payroll/src/Employeecomp.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comp;
-
-public class EmployeeComparer  implements Comparator<Employee> {
-    @Override
-    public int compare(Employee o1, Employee o2) {
-        return o1.getLastName().compareTo(o2.getLastName());
-    }
-}
--- payroll/src/Employeeinfo.java
+++
@@ -1,4 +0,0 @@
-public interface Employeeinfo {
-        double FACULTY_MONTHLY_SALARY = 5000.00;
-        int STAFF_MONTHLY_HOURS_WORKED = 160;
-}
